00f0a9a055573fe67a176ac25684fd83053531a8
tadasi12 <tadasi12@yahoo.co.jp>
2018-08-27 22:15:08 +0900

    new add

---
+++ prj/Django/manager_project/manager/views.py
@@ -0,0 +1,16 @@
+﻿from django.shortcuts import render
+from django.views.generic import TemplateView
+from manager.models import *
+from django.db.models import F, Func
+
+
+class WorkerListView(TemplateView):
+    template_name = "worker_list.html"
+
+    def get(self, request, *args, **kwargs):
+        context = super(WorkerListView, self).get_context_data(**kwargs)
+
+        workers = Worker.objects.all().annotate(distance=Func(10 - F('manager_id'), function='ABS')).order_by("distance")
+        context['workers'] = workers  # context に入れる
+
+        return render(self.request, self.template_name, context)
